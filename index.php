9cc321bb8d628aa38801ef2b3bbea07544b919b7
cecilia8hernandez <zoeh8085@gmail.com>
2024-06-19 09:08:34 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+echo "My first PHP script!";
+echo "<br>";
+echo "Mis datos";
+$nombre="Cecilia Citlalli";
+$edad=19;
+$estatura=1.50;
+echo "Soy ...".strtoupper($nombre)."tengo ".$edad."años y mido". $estatura."metros";
+echo "<br>";
+var_dump(is_int($nombre));
+echo"<br>";
+$x=34;
+$y=356.78;
+$z= $x+$y;
+echo "La suma es $x+ $y=$z <br>";
+$x=35;
+$y=15;
+$z= $x-$y;
+echo "La resta es $x- $y=$z <br>";
+$x=45;
+$y=25;
+$z= $x*$y;
+echo "La multiplicacion es $x * $y=$z <br>";
+$x=70;
+$y=50;
+$z= $x/$y;
+echo "La division es $x / $y=$z <br>";
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica PHP</title>
+</head>
+<body>
+    
+</body>
+</html>
